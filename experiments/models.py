766aed3db737036815aa5839853ad40ce9d7a05c
Ivan Dobrosovestnov <dobrosovestnov@informatics.ru>
2024-03-05 18:24:28 +0100

    Добавил конфигурацию для модели

--- experiments/models.py
+++ experiments/models.py
@@ -1,7 +1,6 @@
 from langchain_openai import ChatOpenAI
-from config.openai_config import OPENAI_API_KEY, MAX_TOKENS_FOR_RESPONSE, OPENAI_ORGANIZATION_ID, OPENAI_TIMEOUT, MAX_RETRIES, OPENAI_SEED
-
 from config.openai_config import *
+from langchain_core.runnables import ConfigurableField
 
 chat_model = ChatOpenAI(
     api_key=OPENAI_API_KEY,
@@ -9,7 +8,9 @@ chat_model = ChatOpenAI(
     max_tokens=MAX_TOKENS_FOR_RESPONSE,
     timeout=TIMEOUT,
     max_retries=MAX_RETRIES,
-    seed=OPENAI_SEED,
     seed=SEED,
     temperature=TEMPERATURE
+).configurable_fields(
+    temperature=ConfigurableField(id="temperature"),
+    model_kwargs=ConfigurableField(id="model_kwargs"),
 )
